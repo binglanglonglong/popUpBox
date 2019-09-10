3d73d5e71c25b0fa74fd9bd9d99d102c614c4062
binglanglonglong <2857480102@qq.com>
2019-09-10 20:51:25 +0800

    vue做的弹框

---
+++ popupbox/src/components/HelloWorld.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+    <button @click="sho">
+      显示
+    </button>
+    <POPUPBOX :msg="msg" :listss="listss"/>
+  </div>
+</template>
+
+<script>
+import POPUPBOX from './popUpBox/index.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    POPUPBOX
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      listss: ["终于知道是什么意思了","针对意义特别强","看我干嘛？我是那种喜欢的吗？","我终于明白了","原来我的感悟这么差"]
+    }
+  },
+  methods: {
+    sho(){
+      this.bus.$emit("comeon",true)
+    }
+  },
+  mounted(){
+    this.bus.$on("clickConfirmation",(res)=>{
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ popupbox/src/components/popUpBox/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-if="shows" class="popupbox">
+        <div class="boxPopupbox">
+            <div class="topline">
+                <span>{{msg}}</span>
+                <span @click="clickCancel">×</span>
+            </div>
+            <div class="textboob">
+                <div v-for="(item,index) in listss" :key="index">
+                    {{item}}
+                </div>
+            </div>
+            <div class="btns">
+                <button @click="clickConfirmation">确定</button>
+                <button @click="clickCancel">取消</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import "./index.css"
+export default {
+    //  msg 是详情, listss 是内容(必须是数组格式)
+    props:["msg","listss"],
+    data(){
+        return {
+            shows: false
+        }
+    },
+    methods: {
+        // 点击确认
+        clickConfirmation(){
+            this.bus.$emit("clickConfirmation",true);
+            this.shows = false;
+        },
+        // 点击取消或×
+        clickCancel(){
+            
+            this.shows = false;
+        }
+        
+    },
+    mounted(){
+        this.bus.$on("comeon",(res)=>{
+            this.shows = res;
+        })
+    }
+}
+</script>
+<style scoped>
+
+</style>
